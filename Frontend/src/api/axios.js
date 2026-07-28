538c4d0fb146cf8f7486c6bfae94265cd7c83020
Yashvi Radariya <yashvi@gmail.com>
2026-07-28 12:08:48 +0530

    Connect frontend to Render backend

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+// const API = axios.create({
+//   baseURL: `${import.meta.env.VITE_API_URL}/api`,
+// });
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
