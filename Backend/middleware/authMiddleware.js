c8257ae8429049efaec0a73dd01d05060d0f0f75
Yashvi Radariya <yashvi@gmail.com>
2026-07-30 14:41:46 +0530

    fix resume latest API

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -2,50 +2,31 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const protect = async (req, res, next) => {
-  try {
+
     let token;
 
-    // Check Authorization Header
     if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
     ) {
-      token = req.headers.authorization.split(" ")[1];
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(
+                token,
+                process.env.JWT_SECRET
+            );
+            req.user = await User.findById(decoded.id)
+                .select("-password");
+            next();
+        } catch(error){
+            return res.status(401).json({
+                message:"Not authorized"
+            });
+        }
+    } else {
+        return res.status(401).json({
+            message:"No token"
+        });
     }
-
-    // No Token
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Not authorized, token missing",
-      });
-    }
-
-    // Verify Token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Get User (without password)
-    req.user = await User.findById(decoded.id).select("-password");
-
-    // User not found
-    if (!req.user) {
-      return res.status(401).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    next();
-  } catch (error) {
-    console.error("Auth Middleware Error:", error.message);
-
-    return res.status(401).json({
-      success: false,
-      message: "Not authorized, invalid token",
-    });
-  }
 };
-
-module.exports = {
-  protect,
-};
+module.exports = protect;
