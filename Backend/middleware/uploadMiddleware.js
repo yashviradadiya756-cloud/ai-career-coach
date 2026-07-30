eaea4dca1be4af2cbb9763ba2f9cd61c133a3dd5
Yashvi Radariya <yashvi@gmail.com>
2026-07-30 14:36:23 +0530

    fix resume latest API

--- Backend/middleware/uploadMiddleware.js
+++ Backend/middleware/uploadMiddleware.js
@@ -2,14 +2,22 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
+    destination:(req,file,cb)=>{
+        cb(null,"uploads/");
+    },
+
+    filename:(req,file,cb)=>{
+        cb(
+            null,
+            Date.now() + path.extname(file.originalname)
+        );
+    }
 });
 
-const upload = multer({ storage });
+
+const upload = multer({
+    storage: storage
+});
+
 
 module.exports = upload;
--- Backend/routes/resumeRoutes.js
+++ Backend/routes/resumeRoutes.js
@@ -1,24 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+const protect = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
+
+console.log("UPLOAD:", upload);
+
 const {
-  uploadResume,
-  getLatestResume
+    uploadResume,
+    getLatestResume
 } = require("../controllers/resumeController");
 
-const protect = require("../middleware/authMiddleware");
 
+// Upload Resume
 router.post(
-  "/upload",
-  protect,
-  upload.single("resume"),
-  uploadResume
+    "/upload",
+    protect,
+    upload.single("resume"),
+    uploadResume
 );
 
+
+// Get Latest Resume
 router.get(
-  "/latest",
-  protect,
-  getLatestResume
+    "/latest",
+    protect,
+    getLatestResume
 );
+
 
 module.exports = router;
