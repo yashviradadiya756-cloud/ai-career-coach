47d7a5d59f3dcb8a188648dc2d152ed51dd4f1f1
Yashvi Radariya <yashvi@gmail.com>
2026-07-30 22:05:20 +0530

    fix resume latest API

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -2,31 +2,33 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const protect = async (req, res, next) => {
-
+  try {
     let token;
 
     if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer")
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
     ) {
-        try {
-            token = req.headers.authorization.split(" ")[1];
-            const decoded = jwt.verify(
-                token,
-                process.env.JWT_SECRET
-            );
-            req.user = await User.findById(decoded.id)
-                .select("-password");
-            next();
-        } catch(error){
-            return res.status(401).json({
-                message:"Not authorized"
-            });
-        }
-    } else {
-        return res.status(401).json({
-            message:"No token"
-        });
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      return next();
     }
+
+    return res.status(401).json({
+      message: "Not authorized",
+    });
+
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
 };
-module.exports = protect;
+
+module.exports = {
+  protect,
+};
--- Backend/routes/resumeRoutes.js
+++ Backend/routes/resumeRoutes.js
@@ -1,35 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-const protect = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authMiddleware");
 const upload = require("../middleware/uploadMiddleware");
 
-console.log("UPLOAD:", upload);
-
 const {
-    uploadResume,
-    getLatestResume
+  uploadResume,
+  getLatestResume,
 } = require("../controllers/resumeController");
 
-console.log("protect:", typeof protect);
-console.log("upload:", typeof upload);
-console.log("uploadResume:", typeof uploadResume);
-console.log("getLatestResume:", typeof getLatestResume);
-// Upload Resume
 router.post(
-    "/upload",
-    protect,
-    upload.single("resume"),
-    uploadResume
+  "/upload",
+  protect,
+  upload.single("resume"),
+  uploadResume
 );
 
-
-// Get Latest Resume
 router.get(
-    "/latest",
-    protect,
-    getLatestResume
+  "/latest",
+  protect,
+  getLatestResume
 );
-
 
 module.exports = router;
