524b6c1cd363e009d69349d07b9b4cacb5bf94f2
Yashvi Radariya <yashvi@gmail.com>
2026-08-07 11:57:16 +0530

    Add assessment dashboard API

--- Frontend/src/api/learningApi.js
+++ Frontend/src/api/learningApi.js
@@ -1,10 +1,12 @@
 import api from "./axios";
 
+// Get latest learning recommendations
 export const getLearning = async () => {
   const response = await api.get("/api/learning");
   return response.data;
 };
 
+// Generate new learning recommendations
 export const generateLearning = async (targetRole) => {
   const response = await api.post(
     "/api/learning/generate",
